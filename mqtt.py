192a8761c2a95680955c4af96c55ba2102cf12f2
Force <naphatnaphat85@gmail.com>
2024-04-03 11:59:46 +0700

    Add battery validate

--- mqtt.py
+++ mqtt.py
@@ -28,6 +28,7 @@ class MQTT():
         self.influxclient_2 = InfluxDBClient(url=self.url, token=self.token, org=self.org, timeout=30000)
         self.write_api = self.influxclient.write_api(write_options=SYNCHRONOUS)
         self.write_api_2 = self.influxclient_2.write_api(write_options=SYNCHRONOUS)
+        self.check_influx_connection()
         self.client.on_connect = self.on_connect
         self.client.on_subscribe = self.on_subscribe
         self.client.on_message = self.on_message
@@ -102,10 +103,12 @@ class MQTT():
         raw_message = json.loads(raw_message)
         topic = message.topic
         
-        print(f"{raw_message}")
-
         #send Env data to InfluxDB
         if topic == "application/0f50f6ff-338d-4dcd-a522-5cfd24f6ffe7/device/24e124136d156431/event/up":
+            if not raw_message["object"]:
+                raw_message["object"]["humidity"] = 0
+                raw_message["object"]["temperature"] = 0
+
             point = {
                 "measurement": "env_monitoring",
                 "tags": {
@@ -122,60 +125,46 @@ class MQTT():
 
             self.write_api.write(self.bucket, self.org, point)
 
-            #publish to node-red
-            # point = {
-            #     "measurement": "env_monitoring",
-            #     "tags": {
-            #         "deviceName":raw_message["deviceInfo"]["deviceName"],
-            #         "devEUI": raw_message["deviceInfo"]["devEui"],
-            #         "deviceClassEnabled": raw_message["deviceInfo"]["deviceClassEnabled"],
-
-            #     },
-            #     "fields": {
-            #         "humidity": raw_message["object"]["humidity"],
-            #         "temperature": raw_message["object"]["temperature"],
-            #     }
-            # }
-
-            # client.publish("env_monitoring", json.dumps(point))
-            # time.sleep(1)
-
         if topic == "application/18ab31a6-9a0d-4a38-a5e3-b3605a9a309f/device/24e124445d186700/event/up":
-            if "gpio_out_1" in raw_message["object"]:
-                point = {
-                    "measurement": "iot_controller",
-                    "tags": {
-                        "deviceName":raw_message["deviceInfo"]["deviceName"],
-                        "devEUI": raw_message["deviceInfo"]["devEui"],
-                        "deviceClassEnabled": raw_message["deviceInfo"]["deviceClassEnabled"],
-                    },
-                    "fields": {
-                        "gpio_out_1": raw_message["object"]["gpio_out_1"],
+            if "battery" in raw_message["object"]:
+                pass
+            else:
+                if "gpio_out_1" in raw_message["object"]:
+                    point = {
+                        "measurement": "iot_controller",
+                        "tags": {
+                            "deviceName":raw_message["deviceInfo"]["deviceName"],
+                            "devEUI": raw_message["deviceInfo"]["devEui"],
+                            "deviceClassEnabled": raw_message["deviceInfo"]["deviceClassEnabled"],
+                        },
+                        "fields": {
+                            "gpio_out_1": raw_message["object"]["gpio_out_1"],
+                        }
                     }
-                }
 
-                self.write_api.write(self.bucket, self.org, point)
-            elif "gpio_out_2" in raw_message["object"]:
-                point = {
-                    "measurement": "iot_controller",
-                    "tags": {
-                        "deviceName":raw_message["deviceInfo"]["deviceName"],
-                        "devEUI": raw_message["deviceInfo"]["devEui"],
-                        "deviceClassEnabled": raw_message["deviceInfo"]["deviceClassEnabled"],
-                    },
-                    "fields": {
-                        "gpio_out_2": raw_message["object"]["gpio_out_2"],
+                    self.write_api.write(self.bucket, self.org, point)
+                elif "gpio_out_2" in raw_message["object"]:
+                    point = {
+                        "measurement": "iot_controller",
+                        "tags": {
+                            "deviceName":raw_message["deviceInfo"]["deviceName"],
+                            "devEUI": raw_message["deviceInfo"]["devEui"],
+                            "deviceClassEnabled": raw_message["deviceInfo"]["deviceClassEnabled"],
+                        },
+                        "fields": {
+                            "gpio_out_2": raw_message["object"]["gpio_out_2"],
+                        }
                     }
-                }
 
-                self.write_api.write(self.bucket, self.org, point)
+                    self.write_api.write(self.bucket, self.org, point)
 
         if topic == "application/c22e4c79-d61a-4e8e-bccd-e39384c913f9/device/24e124141d223816/event/up":
             if "install" in raw_message["object"]:
                 pass
+            elif "battery" in raw_message["object"]:
+                pass
 
             else:
-                print("*"*100)
                 if raw_message["object"]["state"] == "open":
                     raw_message["object"]["state"] = 1
                 elif raw_message["object"]["state"] == "close":
@@ -196,73 +185,79 @@ class MQTT():
                 self.write_api.write(self.bucket, self.org, point)
 
         if topic == "application/b50618fe-3073-4e6c-acaf-b1b2ff3af9bf/device/24e124538d222160/event/up":
-            if raw_message["object"]["pir"] == "normal":
-                raw_message["object"]["pir"] = 0
-            elif raw_message["object"]["pir"] == "trigger":
-                raw_message["object"]["pir"] = 1
+            if "battery" in raw_message["object"]:
+                pass
+            else:
+                if raw_message["object"]["pir"] == "normal":
+                    raw_message["object"]["pir"] = 0
+                elif raw_message["object"]["pir"] == "trigger":
+                    raw_message["object"]["pir"] = 1
 
-            point = {
-                "measurement": "pir",
-                "tags": {
-                    "deviceName":raw_message["deviceInfo"]["deviceName"],
-                    "devEUI": raw_message["deviceInfo"]["devEui"],
-                    "deviceClassEnabled": raw_message["deviceInfo"]["deviceClassEnabled"],
-                },
-                "fields": {
-                    "pir": raw_message["object"]["pir"],
+                point = {
+                    "measurement": "pir",
+                    "tags": {
+                        "deviceName":raw_message["deviceInfo"]["deviceName"],
+                        "devEUI": raw_message["deviceInfo"]["devEui"],
+                        "deviceClassEnabled": raw_message["deviceInfo"]["deviceClassEnabled"],
+                    },
+                    "fields": {
+                        "pir": raw_message["object"]["pir"],
+                    }
                 }
-            }
 
-            self.write_api.write(self.bucket, self.org, point)
+                self.write_api.write(self.bucket, self.org, point)
 
         if topic == "application/99cedc9c-2c85-44a0-9889-63ac556fdeac/device/24e124148c406038/event/up":
-            if "factor" in raw_message["object"]:
-                point = {
-                    "measurement": "portable_socket",
-                    "tags": {
-                        "deviceName":raw_message["deviceInfo"]["deviceName"],
-                        "devEUI": raw_message["deviceInfo"]["devEui"],
-                        "deviceClassEnabled": raw_message["deviceInfo"]["deviceClassEnabled"],
-                    },
-                    "fields": {
-                        "factor": raw_message["object"]["factor"],
-                        "current": raw_message["object"]["current"],
-                        "state": raw_message["object"]["state"],
+            if "battery" in raw_message["object"]:
+                pass
+            else:
+                if "factor" in raw_message["object"]:
+                    point = {
+                        "measurement": "portable_socket",
+                        "tags": {
+                            "deviceName":raw_message["deviceInfo"]["deviceName"],
+                            "devEUI": raw_message["deviceInfo"]["devEui"],
+                            "deviceClassEnabled": raw_message["deviceInfo"]["deviceClassEnabled"],
+                        },
+                        "fields": {
+                            "factor": raw_message["object"]["factor"],
+                            "current": raw_message["object"]["current"],
+                            "state": raw_message["object"]["state"],
+                        }
                     }
-                }
 
-                self.write_api.write(self.bucket, self.org, point)
+                    self.write_api.write(self.bucket, self.org, point)
 
-            elif "powersum" in raw_message["object"]:
-                point = {
-                    "measurement": "portable_socket",
-                    "tags": {
-                        "deviceName":raw_message["deviceInfo"]["deviceName"],
-                        "devEUI": raw_message["deviceInfo"]["devEui"],
-                        "deviceClassEnabled": raw_message["deviceInfo"]["deviceClassEnabled"],
-                    },
-                    "fields": {
-                        "powersum": raw_message["object"]["powersum"],
-                        "voltage": raw_message["object"]["voltage"],
+                elif "powersum" in raw_message["object"]:
+                    point = {
+                        "measurement": "portable_socket",
+                        "tags": {
+                            "deviceName":raw_message["deviceInfo"]["deviceName"],
+                            "devEUI": raw_message["deviceInfo"]["devEui"],
+                            "deviceClassEnabled": raw_message["deviceInfo"]["deviceClassEnabled"],
+                        },
+                        "fields": {
+                            "powersum": raw_message["object"]["powersum"],
+                            "voltage": raw_message["object"]["voltage"],
+                        }
                     }
-                }
 
-                self.write_api.write(self.bucket, self.org, point)
+                    self.write_api.write(self.bucket, self.org, point)
 
-            elif "power" in raw_message["object"]:
-                point = {
-                    "measurement": "portable_socket",
-                    "tags": {
-                        "deviceName":raw_message["deviceInfo"]["deviceName"],
-                        "devEUI": raw_message["deviceInfo"]["devEui"],
-                        "deviceClassEnabled": raw_message["deviceInfo"]["deviceClassEnabled"],
-                    },
-                    "fields": {
-                        "power": raw_message["object"]["power"],
+                elif "power" in raw_message["object"]:
+                    point = {
+                        "measurement": "portable_socket",
+                        "tags": {
+                            "deviceName":raw_message["deviceInfo"]["deviceName"],
+                            "devEUI": raw_message["deviceInfo"]["devEui"],
+                            "deviceClassEnabled": raw_message["deviceInfo"]["deviceClassEnabled"],
+                        },
+                        "fields": {
+                            "power": raw_message["object"]["power"],
+                        }
                     }
-                }
 
-                self.write_api.write(self.bucket, self.org, point)
+                    self.write_api.write(self.bucket, self.org, point)
 
         if topic == "application/9d59b0e3-5210-4763-9fb3-a6fdc8d511f4/device/24e124535d229796/event/up":
             if "battery" in raw_message["object"]:
@@ -323,23 +318,7 @@ class MQTT():
         topics = [(f"application/{app_id_env}/device/{dev_eui_env}/event/up",0), (f"application/{app_id_iot_controller}/device/{dev_eui_iot_controller}/event/up",0), (f"application/{app_id_mag_contract_switch}/device/{dev_eui_mag_contract_switch}/event/up",0), (f"application/{app_id_pir}/device/{dev_eui_pir}/event/up",0), (f"application/{app_id_portable_socket}/device/{dev_eui_portable_socket}/event/up",0), (f"application/{app_id_smart_button}/device/{dev_eui_smart_button}/event/up",0), (f"application/{app_id_sound_level}/device/{dev_eui_sound_level}/event/up",0)]
 
         for topic in topics:
-            print(topic)
             self.client.subscribe(topic)
-        #Env
-        # self.client.subscribe("application/0f50f6ff-338d-4dcd-a522-5cfd24f6ffe7/device/24e124136d156431/event/up")
-        # #Magnetic Contract Switch
-        # self.client.subscribe("application/c22e4c79-d61a-4e8e-bccd-e39384c913f9/device/24e124141d223816/event/up")
-        # #PIR
-        # self.client.subscribe("application/b50618fe-3073-4e6c-acaf-b1b2ff3af9bf/device/24e124538d222160/event/up")
-        # #Portable Socket
-        # self.client.subscribe("application/99cedc9c-2c85-44a0-9889-63ac556fdeac/device/24e124148c406038/event/up")
-        # #Smart Button
-        # self.client.subscribe("application/9d59b0e3-5210-4763-9fb3-a6fdc8d511f4/device/24e124535d229796/event/up")
-        # #Sound Level
-        # self.client.subscribe("application/2b695811-907e-4e92-b1c7-00d2ca2dc3c7/device/24e124743d015542/event/up")
-        # #Iot Controller
-        # self.client.subscribe("application/18ab31a6-9a0d-4a38-a5e3-b3605a9a309f/device/24e124445d186700/event/up")
-            
 
     def check_influx_connection(self):
         try:
